8023c2dd68725bd7815d052d6f7b5df9f7202234
Natalia Demkiv <mnatao1@gmail.com>
2026-02-15 23:47:32 +0100

    delete maze

--- Maze/maze/errors.py
+++
@@ -1,9 +0,0 @@
-"""Project-specific exceptions."""
-
-
-class ConfigError(Exception):
-    """Raised when the configuration file is invalid."""
-
-
-class MazeError(Exception):
-    """Raised when maze generation/solving fails."""
